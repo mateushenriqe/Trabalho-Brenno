88a1c4ab31fbaa478b84eccb1d828306d007dd2e
mateushenriqe <153244600+mateushenriqe@users.noreply.github.com>
2023-12-13 19:53:00 -0300

    Add files via upload

---
+++ Cabecalho.java
@@ -0,0 +1,19 @@
+package Quiz;
+
+public class Cabecalho {
+
+    String tema = "Quizz";
+    String aluno = "nome aluno";
+    String professor = "Brenno Pimenta";
+    String materia = "Algoritmo e programação 2";
+    String curso = "engenharia de software";
+
+    public  void Cabecalho(){
+
+        System.out.println(this.tema);
+        System.out.println(this.aluno);
+        System.out.println(this.professor);
+        System.out.println(this.materia);
+        System.out.println(this.curso);
+    }
+}
